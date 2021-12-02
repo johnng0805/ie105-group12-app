7ea7f58e74d1b06ee5e1b8798ad3ea6244a026b5
johnng0805 <johnng0805@gmail.com>
2021-12-02 15:07:40 +0700

    Secure cookie.

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
@@ -26,5 +27,23 @@ class AuthController extends Controller
     public function show()
     {
         return view("login");
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), $this->ruleRegister);
+        if ($validator->fails()) {
+            return response()->json(["error" => $validator->errors()], 400);
+        }
+
+        $user = new User;
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = password_hash($request->password, PASSWORD_BCRYPT);
+
+        $user->save();
+
+        return response()->json(["status" => "success"], 200);
     }
 }
